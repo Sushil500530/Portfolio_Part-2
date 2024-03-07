9348208f3f7031844d2257651906a361dfc6fa17
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-07 18:25:43 +0600

    add tailwind,daisyui and router whit some components

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className="text-center text-2xl font-bold">Home is Comming......!</h2>
+        </div>
+    );
+};
+
+export default Home;
