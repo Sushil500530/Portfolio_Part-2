53e2b42c8fae6e8a613ef2839933739bcc854b42
Sushil Hemrom <sushilhemrom98@gmail.com>
2024-03-15 20:05:08 +0600

    add title logo and other

--- src/components/shared/Preloader.jsx
+++ src/components/shared/Preloader.jsx
@@ -2,10 +2,10 @@ import Image3 from "../../assets/image/bouncing-ball.svg";
 
 const Preloader = () => {
     return (
-        <div className="w-full h-screen bg-gradient-to-r from-[#2f2f2f] to-[#500c7d] flex items-center justify-center flex-col">
+        <div className="w-full h-screen bg-gradient-to-t from-[#1e1d1d] to-[#03046f] flex items-center justify-center flex-col">
             <div className="w-1/2 h-[50vh] mx-auto relative">
                 <img src={Image3} alt="loader" className="bg-transparent w-full h-full" />
-                <h1 className="text-white absolute bottom-7 left-0 right-0 text-center text-7xl animate-pulse font-bold">Loading.....</h1>
+                <h1 className="text-white absolute bottom-7 left-0 right-0 text-center text-3xl lg:text-7xl animate-pulse font-bold">Loading.....</h1>
             </div>
         </div>
     );
